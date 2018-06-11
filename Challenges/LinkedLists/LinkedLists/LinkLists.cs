a7df560d50a2accd94a64fbbc4c728ed190f0591
Judah Hunger <judah.hunger@gmail.com>
2018-06-11 16:41:43 -0700

    Added tests and methods from class to LinkedLists Challenge.

--- Challenges/LinkedLists/LinkedLists/LinkLists.cs
+++ Challenges/LinkedLists/LinkedLists/LinkLists.cs
@@ -4,12 +4,13 @@ using System.Text;
 
 namespace LinkedLists
 {
-    class LinkLists
+    public class LinkLists
     {
         public Node Head { get; set; }
+
         public Node Current { get; set; }
 
-        public LinkList(Node node)
+        public LinkLists(Node node)
         {
             Head = node;
             Current = node;
@@ -20,6 +21,74 @@ namespace LinkedLists
             node.Next = Head;
             Head = node;
             Current = node;
+        }
+
+        public void AddBefore(Node newNode, Node existingNode)
+        {
+            Current = Head;
+            if (Head.Value == existingNode.Value)
+            {
+                Add(newNode);
+                return;
+            }
+            while (Current.Next != null)
+            {
+                if (Current.Next.Value == existingNode.Value)
+                {
+                    newNode.Next = existingNode;
+                    Current.Next = newNode;
+                    return;
+                }
+            }
+
+            Current = Current.Next; 
+        }
+
+        /*public void AddAfter(Node newNode, Node existingNode)
+        {
+            Current = Head;
+            while (true)
+            {
+
+            }
+        }*/
+
+        public void AddLast(Node newNode)
+        {
+            Current = Head;
+
+            while (Current.Next != null)
+            {
+                Current = Current.Next;
+            }
+            Current.Next = newNode;
+        }
+
+        public Node Find(int value)
+        {
+            Current = Head;
+
+            while (Current.Next != null)
+            {
+                if (Current.Value == value)
+                {
+                    return Current;
+                }
+                Current = Current.Next;
+            }
+            return Current.Value == value ? Current : null;
+        }
+        
+        public void Print()
+        {
+            Current = Head;
+
+            while (Current.Next != null)
+            {
+                Console.Write($"{Current.Value}--->");
+                Current = Current.Next;
+            }
+            Console.Write($"{Current.Value}---> null");
         }
     }
 }

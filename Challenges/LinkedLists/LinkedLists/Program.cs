09fc36ae504c8e8bb91764fce7e4fbc36b6f93e9
Judah Hunger <judah.hunger@gmail.com>
2018-06-11 13:57:50 -0700

    Linked List initial push

---
+++ Challenges/LinkedLists/LinkedLists/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using LinkedLists;
+
+namespace LinkedLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void TestLL()
+        {
+            LinkList ll = new LinkList(new Node(10));
+
+        }
+
+    }
+}
